6e2fadb77192177dbf19a00822c0829b563e7dcc
Minh Tuấn <vietkeynet@gmail.com>
2026-09-17 11:22:57 +0700

    feat: add motion activity signal

--- core/src/commonMain/kotlin/org/traccar/client/Signal.kt
+++ core/src/commonMain/kotlin/org/traccar/client/Signal.kt
@@ -1,6 +1,7 @@
 package org.traccar.client
 
 sealed interface Signal {
+    data class MotionChanged(val activity: MotionActivity) : Signal
     object StationaryEnter : Signal
     object StationaryExit : Signal
     object HeartbeatTick : Signal
